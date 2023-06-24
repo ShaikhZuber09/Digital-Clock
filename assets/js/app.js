97a8b3d0988584f775a80ba3a8bb6d31ec045a8a
ShaikhZuber09~ <zs618272@gmail.com>
2023-06-24 03:37:46 -0400

    first

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+function SetZero(num){
+    if(num<10){
+       return '0'+ num
+    }
+    else{
+        return num;
+    }
+}
+function CreateDigitalClock(){
+    const clock=document.getElementById("clock")
+
+    let d=new Date();
+    let hr =d.getHours();
+    let min=d.getMinutes();
+    let sec=d.getSeconds();
+    let session="AM";
+    if(hr>12){
+        session="PM";
+        hr=hr-12;
+    }
+   
+    
+    
+    
+    clock.innerHTML=`${ SetZero(hr)}:${SetZero(min)}:${SetZero(sec)}`
+    setTimeout(CreateDigitalClock, 300)
+}
+
+CreateDigitalClock();
